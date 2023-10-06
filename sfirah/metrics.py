63305765e92a6b2c27d2b3fd84fc3b8ae7ef4ae9
jopetty <research@jacksonpetty.org>
2023-10-05 21:50:59 -0400

    Add CE loss wrapper; make accuracy methods more forgiving

--- sfirah/metrics.py
+++ sfirah/metrics.py
@@ -2,21 +2,35 @@
 
 
 from torch import Tensor
+from torch.nn import functional as F  # noqa: N812
+
+
+def ce_loss(predictions: Tensor, targets: Tensor) -> float:
+    """Compute the cross-entropy loss.
+
+    Args:
+        predictions (Tensor): The predictions.
+        targets (Tensor): The targets.
+    """
+    return F.cross_entropy(predictions.argmax(dim=1), targets).mean().item()
 
 
 def token_accuracy(predictions: Tensor, targets: Tensor) -> float:
     """Compute the per-token accuracy of a batch of predictions.
 
     Computes the number of correctly-predicted tokens divided by the
-    total number of tokens. Note that `predictions` and `targets` must
-    have the same shape---i.e., you should `argmax()` the predictions before
-    passing them to this function.
+    total number of tokens. The `predictions` can be passed in as logits
+    or as tokens; if passed as logits, `argmax(dim=1)` will be applied.
 
     Args:
         predictions (Tensor): The predictions.
         targets (Tensor): The targets.
     """
+    if predictions.dim() != targets.dim():
+        predictions = predictions.argmax(dim=1)
+
     assert predictions.size() == targets.size()
+    assert predictions.dim() == 2
 
     return (predictions == targets).float().mean().item()
 
@@ -25,14 +39,16 @@ def sequence_accuracy(predictions: Tensor, targets: Tensor) -> float:
     """Compute the per-sequence accuracy of a batch of predictions.
 
     Computes the number of correctly-predicted sequences divided by the
-    total number of sequences. Note that `predictions` and `targets` must
-    have the same shape---i.e., you should `argmax()` the predictions before
-    passing them to this function.
+    total number of sequences. The `predictions` can be passed in as logits
+    or as tokens; if passed as logits, `argmax(dim=1)` will be applied.
 
     Args:
         predictions (Tensor): The predictions.
         targets (Tensor): The targets.
     """
+    if predictions.dim() != targets.dim():
+        predictions = predictions.argmax(dim=1)
+
     assert predictions.size() == targets.size()
     assert predictions.dim() == 2
 
