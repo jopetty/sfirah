73c482bbcdf7996df98d7b81476419c3cf21cbf9
jopetty <research@jacksonpetty.org>
2024-02-26 14:43:13 -0500

    fix topk?

--- sfirah/transformers.py
+++ sfirah/transformers.py
@@ -408,7 +408,7 @@ class GenerativeDecoder:
             else:
                 logits = logits[:, -1, :] / temperature
                 if top_k is not None:
-                    v, _ = torch.topk(logits, top_k=min(top_k, logits.shape[-1]))
+                    v, _ = torch.topk(logits, k=min(top_k, logits.shape[-1]))
                     logits[logits < v[:, [-1]]] = -float("Inf")
 
                 probs = F.softmax(logits, dim=1)
