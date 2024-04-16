071f29b7880beccc38fba72545b820798e8c9692
jopetty <research@jacksonpetty.org>
2024-04-16 16:43:14 -0400

    check equivalence between fast and slow versions

--- sfirah/ids4.py
+++ sfirah/ids4.py
@@ -5,7 +5,8 @@ from itertools import accumulate
 import torch
 from einops import einsum
 from torch import Tensor, nn
-from torch.nn import functional as F  # noqa: N812
+
+from .utils import get_activation
 
 
 class IDS4Block(nn.Module):
@@ -19,11 +20,16 @@ class IDS4Block(nn.Module):
     def d_model(self) -> int:  # noqa: D102
         return self._d_model
 
+    @property
+    def activation(self) -> str:  # noqa: D102
+        return self._activation
+
     def __init__(  # noqa: D107
-        self, d_state: int, d_model: int
+        self, activation: str, d_state: int, d_model: int
     ):
         super().__init__()
 
+        self._activation = activation
         self._d_state = d_state
         self._d_model = d_model
 
@@ -35,6 +41,24 @@ class IDS4Block(nn.Module):
         self.C = nn.Linear(self.d_state, self.d_model)
         self.D = nn.Linear(self.d_model, self.d_model)
 
+        self.activation_fn = get_activation(activation, functional=True)
+
+    def forward_slow(self, x: Tensor) -> Tensor:
+        """Forward pass (slow version)."""
+        Ax = einsum(x, self.A, "b l dm, dm ds dst -> b l ds dst")  # noqa: N806
+        Ax = self.activation_fn(Ax)  # noqa: N806  (B, L, d_state, d_state)
+
+        cum_prod = []
+        for i in range(Ax.shape[0]):
+            A_i = torch.unbind(Ax[i], dim=0)  # noqa: N806 [(d_state, d_state), ...] * L
+            prod_list = list(accumulate(A_i, lambda x, y: x @ y))
+            prod_list = [self.proj(p) for p in prod_list]
+            A_i = torch.stack(prod_list, dim=0).squeeze()  # noqa: N806 (L, d_state)
+            cum_prod.append(A_i)
+        cum_prod = torch.stack(cum_prod, dim=0).squeeze()  # (B, L, d_state)
+
+        return self.C(cum_prod) + self.D(x)
+
     def forward(self, x: Tensor) -> Tensor:
         """Forward pass.
 
@@ -42,7 +66,7 @@ class IDS4Block(nn.Module):
             x: (B, L, d_model)
         """
         Ax = einsum(x, self.A, "b l dm, dm ds dst -> b l ds dst")  # noqa: N806
-        Ax = F.gelu(Ax)  # noqa: N806  (B, L, d_state, d_state)
+        Ax = self.activation_fn(Ax)  # noqa: N806  (B, L, d_state, d_state)
 
         Ax_i = torch.unbind(Ax, dim=1)  # noqa: N806
         cum_prod = list(accumulate(Ax_i, lambda x, y: torch.bmm(x, y)))
@@ -84,12 +108,17 @@ class IDS4TokenClassifier(nn.Module):
         return self._prenorm
 
     @property
+    def activation(self) -> str:  # noqa: D102
+        return self._activation
+
+    @property
     def num_parameters(self) -> int:
         """Return the total number of trainable parameters."""
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
 
     def __init__(  # noqa: D107
         self,
+        activation: str,
         d_model: int,
         d_state: int,
         n_layers: int,
@@ -100,6 +129,7 @@ class IDS4TokenClassifier(nn.Module):
     ):
         super().__init__()
 
+        self._activation = activation
         self._bias = bias
         self._d_model = d_model
         self._n_vocab = n_vocab
@@ -114,13 +144,41 @@ class IDS4TokenClassifier(nn.Module):
         self.dropout_layers = nn.ModuleList()
 
         for _ in range(n_layers):
-            self.s4_layers.append(IDS4Block(d_model=self.d_model, d_state=self.d_state))
+            self.s4_layers.append(
+                IDS4Block(
+                    d_model=self.d_model,
+                    d_state=self.d_state,
+                    activation=self.activation,
+                )
+            )
             self.norms.append(nn.LayerNorm(self.d_model))
             self.dropout_layers.append(nn.Dropout(self.dropout))
 
         self.cl_head = nn.Linear(self.d_model, self.n_vocab, self.bias)
 
     def forward(self, x: Tensor) -> Tensor:  # noqa: D102
+        x = self.embedding(x)  # (B, L, d_input) -> (B, L, d_model)
+
+        for layer, norm, dropout in zip(
+            self.s4_layers, self.norms, self.dropout_layers
+        ):
+            z = x
+            if self.prenorm:
+                z = norm(z)
+
+            z = layer(z)
+            z = dropout(z)
+            x = x + z
+
+            if not self.prenorm:
+                x = norm(x)
+
+        x = self.cl_head(x)
+
+        return x
+
+    def forward_slow(self, x: Tensor) -> Tensor:
+        """Forward pass (slow version)."""
         x = self.embedding(x)  # (B, L, d_input) -> (B, L, d_model)
         # x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
 
@@ -131,7 +189,7 @@ class IDS4TokenClassifier(nn.Module):
             if self.prenorm:
                 z = norm(z)
 
-            z = layer(z)
+            z = layer.forward_slow(z)
             z = dropout(z)
             x = x + z
 
--- tests/test_ids4.py
+++ tests/test_ids4.py
@@ -1,6 +1,7 @@
 """Tests for the IDS4 model."""
 
 import unittest
+from itertools import accumulate
 
 import torch
 
@@ -10,13 +11,73 @@ from sfirah import ids4
 class TestIDS4TokenClassifier(unittest.TestCase):  # noqa: D101
     def test_init(self):  # noqa: D102
         _ = ids4.IDS4TokenClassifier(
-            d_model=10, d_state=20, n_vocab=30, dropout=0.1, n_layers=1
+            d_model=10,
+            d_state=20,
+            n_vocab=30,
+            dropout=0.1,
+            n_layers=1,
+            activation="gelu",
         )
 
     def test_forward(self):  # noqa: D102
         model = ids4.IDS4TokenClassifier(
-            d_model=10, d_state=20, n_vocab=30, dropout=0.1, n_layers=1
+            d_model=10,
+            d_state=20,
+            n_vocab=30,
+            dropout=0.1,
+            n_layers=1,
+            activation="gelu",
         )
 
         x = torch.ones([2, 4], dtype=torch.int64)
         _ = model(x)
+
+    def test_fast_forward(self):  # noqa: D102
+        model = ids4.IDS4TokenClassifier(
+            d_model=5, d_state=6, n_vocab=30, dropout=0.0, n_layers=1, activation="gelu"
+        )
+
+        x = torch.ones([2, 4], dtype=torch.int64)
+        _ = model(x)
+
+    def check_fast_slow_equivalent(self):
+        """Check if the outputs of a multi-layer IDS4 model are equivalent.
+
+        Uses the actualy implementation of the IDS4 fast and slow forward passes.
+        """
+        model = ids4.IDS4TokenClassifier(
+            d_model=5, d_state=6, n_vocab=30, dropout=0.0, n_layers=1, activation="gelu"
+        )
+
+        x = torch.ones([2, 4], dtype=torch.int64)
+
+        with torch.no_grad():
+            x_slow = model.forward_slow(x)
+            x_fast = model(x)
+
+        self.assertTrue(torch.allclose(x_fast, x_slow))
+
+    def test_cum_bmm(self):
+        """Manual test for cummulative bmm operation.
+
+        This exists as a sanity check to ensure that the fast and slow versions
+        of the IDS4 matrix multiplication are equivalent.
+        """
+        ax = torch.randn(2, 4, 6, 6)
+        proj = torch.nn.Linear(6, 1)
+
+        cum_prod_slow = []
+        for i in range(ax.shape[0]):
+            ax_i = torch.unbind(ax[i], dim=0)
+            prod_list = list(accumulate(ax_i, lambda x, y: x @ y))
+            prod_list = [proj(p) for p in prod_list]
+            ax_i = torch.stack(prod_list, dim=0).squeeze()
+            cum_prod_slow.append(ax_i)
+        cum_prod_slow = torch.stack(cum_prod_slow, dim=0).squeeze()
+
+        ax_i = torch.unbind(ax, dim=1)
+        cum_prod_fast = list(accumulate(ax_i, lambda x, y: torch.bmm(x, y)))
+        cum_prod_fast = [proj(p) for p in cum_prod_fast]
+        cum_prod_fast = torch.stack(cum_prod_fast, dim=1).squeeze()
+
+        self.assertTrue(torch.allclose(cum_prod_fast, cum_prod_slow))
